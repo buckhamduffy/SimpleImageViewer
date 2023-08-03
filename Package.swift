b462f1fac65c2ce46f7474e2f450710b43e9d4fa
zane-carter-sv <hello@zanecarter.com>
2023-08-03 12:51:12 +1000

    Updated to support SPM

--- Package.swift
+++ Package.swift
@@ -1,6 +1,18 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "SimpleImageViewer",
-    dependencies : []
+    products: [
+        .library(
+            name: "SimpleImageViewer",
+            targets: ["SimpleImageViewer"]),
+    ],
+    targets: [
+        .target(
+            name: "SimpleImageViewer",
+            dependencies: [])
+    ]
 )
